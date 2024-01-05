0db8f220bd55e3e5f7f5c9ef28e19245fdf4420f
lumpyspaceyz <yzever99@gmail.com>
2024-01-06 00:55:04 +0900

    [modify] header 컴포넌트 서브 메뉴 마크업과 스타일링 추가 및 스크롤 이벤트 JS 추가

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,2 +1,15 @@
 import '/src/styles/style.css';
 import '/src/components/header/header.css';
+import { getNode, css } from '/src/lib';
+
+(function () {
+  const navScroll = getNode('.nav_scroll');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 190) {
+      css(navScroll, 'display', 'flex');
+    } else if (window.scrollY < 190) {
+      css(navScroll, 'display', 'none');
+    }
+  });
+})();
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 // import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: 'docs',
   },
-})
+  css: {
+    devSourcemap: true,
+  },
+});
