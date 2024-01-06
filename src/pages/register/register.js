6b7803f7beadebb643a7eceff99f7318e60a22c5
song0331 <thdgusrb@gmail.com>
2024-01-06 20:53:19 +0900

    [add] 회원가입 페이지 input에 값이 있다면 글자 검은색으로 변하는 기능

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -2,3 +2,42 @@ import '/src/styles/style.css';
 import '/src/components/header/header.css';
 import '/src/pages/register/register.css';
 import '/src/components/footer/footer.css';
+import { getNode } from '/src/lib';
+
+/*
+.user_id_input,
+.user_password_input,
+.user_password_check_input,
+.user_name_input,
+.user_email_input,
+.user_phone_input
+*/
+
+const userId = getNode('.user_id_input');
+const userPassword_input = getNode('.user_password_input');
+const userPasswordCheckInput = getNode('.user_password_check_input');
+const userNameInput = getNode('.user_name_input');
+const userEmailInput = getNode('.user_email_input');
+const userPhoneInput = getNode('.user_phone_input');
+
+// input에 값이 있다면 글자가 검은색으로 변함
+function handleInputActive(e, node) {
+  if (e.target.value) node.classList.add('input_active');
+}
+
+userId.addEventListener('input', (e) => handleInputActive(e, userId));
+userPassword_input.addEventListener('input', (e) =>
+  handleInputActive(e, userPassword_input)
+);
+userPasswordCheckInput.addEventListener('input', (e) =>
+  handleInputActive(e, userPasswordCheckInput)
+);
+userNameInput.addEventListener('input', (e) =>
+  handleInputActive(e, userNameInput)
+);
+userEmailInput.addEventListener('input', (e) =>
+  handleInputActive(e, userEmailInput)
+);
+userPhoneInput.addEventListener('input', (e) =>
+  handleInputActive(e, userPhoneInput)
+);
