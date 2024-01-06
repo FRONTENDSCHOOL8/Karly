5b3640cbd3e8a51f0d7a82883ae07b92402b0a60
song0331 <thdgusrb@gmail.com>
2024-01-06 16:48:03 +0900

    [add] 회원가입 페이지에 헤더, 푸터 컴포넌트 추가

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,9 +1,4 @@
 import '/src/styles/style.css';
+import '/src/components/header/header.css';
 import '/src/pages/register/register.css';
-import { getNode } from '/src/lib';
-
-const btn = getNode('.check_number_btn');
-console.log(444);
-btn.addEventListener('click', function () {
-  console.log(111);
-});
+import '/src/components/footer/footer.css';
