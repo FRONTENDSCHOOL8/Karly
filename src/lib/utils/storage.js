ff553f9bc831e5ec584d9981da957578abf83c4d
song0331 <thdgusrb@gmail.com>
2024-01-08 19:54:01 +0900

    [add] pocketbase 연결 중

--- src/lib/utils/storage.js
+++ src/lib/utils/storage.js
@@ -1,56 +1,34 @@
-import { isString } from "./typeOf.js"
-import { typeError } from '../error/typeError.js'
+import { isString } from './typeOf.js';
+// eslint-disable-next-line no-unused-vars
+import { typeError } from '../error/typeError.js';
 
+const { localStorage: storage } = window;
 
-
-
-const {localStorage:storage} = window
-
-
-export function setStorage(key,value){
- 
+export function setStorage(key, value) {
   return new Promise((resolve, reject) => {
-    if(isString(key)){
-      storage.setItem(key,JSON.stringify(value))
-      resolve()
-    }else{
-      reject({message:'key는 문자 타입 이어야 합니다.'})
+    if (isString(key)) {
+      storage.setItem(key, JSON.stringify(value));
+      resolve();
+    } else {
+      reject({ message: 'key는 문자 타입 이어야 합니다.' });
     }
-  })
+  });
 }
 
-export function getStorage(key){
-  
+export function getStorage(key) {
   return new Promise((resolve, reject) => {
-    if(isString(key)){
-      resolve(JSON.parse(storage.getItem(key)))
-    }else{
-      reject({message:'key는 문자 타입 이어야 합니다.'})
+    if (isString(key)) {
+      resolve(JSON.parse(storage.getItem(key)));
+    } else {
+      reject({ message: 'key는 문자 타입 이어야 합니다.' });
     }
-  })
+  });
 }
 
-export function deleteStorage(key){
+export function deleteStorage(key) {
+  // eslint-disable-next-line no-unused-vars
   return new Promise((resolve, reject) => {
     !key ? storage.clear() : storage.removeItem(key);
     resolve();
-  })
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,14 +3,41 @@ import '/src/components/header/header.css';
 import '/src/pages/login/login.css';
 import '/src/components/footer/footer.css';
 import { getNode } from '/src/lib';
+import PocketBase from 'pocketbase';
 
-const login_btn = getNode('.login_btn');
-const register_btn = getNode('.register_btn');
+const pb = new PocketBase('http://127.0.0.1:8090');
 
-login_btn.addEventListener('click', function (e) {
+const loginBtn = getNode('.login_btn');
+const userIdInput = getNode('.user_id_input');
+const userPasswordInput = getNode('.user_password_input');
+
+const data = await pb.collection('users').getOne('c9dyw5dd41teztn');
+
+// .authWithPassword(data.username, 'song123456789');
+
+console.log(data);
+// console.log(pb.authStore.isValid);
+// console.log(pb.authStore.token);
+// console.log(pb.authStore.model.id);
+// console.log(data.name);
+
+// setStorage('user_id', data.name);
+
+// 로그인 성공하면 localStorage에 값이 생성된다.
+async function handleAuth(id, pw) {
+  try {
+    const authData = await pb.collection('users').authWithPassword(id, pw);
+    if (authData) {
+      console.log('로그인 성공');
+    }
+    console.log(authData);
+  } catch (error) {
+    console.log('로그인 실패!', error);
+  }
+}
+
+loginBtn.addEventListener('click', function (e) {
   e.preventDefault();
-});
 
-register_btn.addEventListener('click', function (e) {
-  e.preventDefault();
+  handleAuth(userIdInput.value, userPasswordInput.value);
 });
