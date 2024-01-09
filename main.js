5245f0fb58a18cd489d04e894e25e7bd5d2613ce
lumpyspaceyz <yzever99@gmail.com>
2024-01-10 04:40:58 +0900

    [add] main 페이지 배너 슬라이더, 상품 슬라이더 swiper.js 기능 구현

--- main.js
+++ main.js
@@ -2,3 +2,87 @@ import '/src/styles/style.css';
 import '/src/styles/main.css';
 import '/src/components/header/header.css';
 import '/src/components/footer/footer.css';
+import { getNode, css, setDocumentTitle } from '/src/lib/';
+import Swiper from 'swiper';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+setDocumentTitle('칼리 | 마켓칼리');
+
+const swiperBanner = new Swiper('.swiper.banner', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next.banner',
+    prevEl: '.swiper-button-prev.banner',
+  },
+  pagination: {
+    el: '.swiper-pagination.banner',
+    type: 'fraction',
+  },
+});
+
+const swiperRecommend = new Swiper('.swiper.recommend', {
+  modules: [Navigation, Pagination],
+  loop: false,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 30,
+  centeredSlides: false,
+  navigation: {
+    nextEl: '.swiper-button-next.recommend',
+    prevEl: '.swiper-button-prev.recommend',
+  },
+});
+
+const swiperSale = new Swiper('.swiper.sale', {
+  modules: [Navigation, Pagination, Autoplay],
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next.sale',
+    prevEl: '.swiper-button-prev.sale',
+  },
+});
+
+const swiperBannerPrevBtn = getNode('.swiper-button-prev.recommend');
+const swiperBannerNextBtn = getNode('.swiper-button-next.recommend');
+
+function handleSliderBtn() {
+  if (swiperBannerNextBtn.classList.contains('swiper-button-disabled')) {
+    css(swiperBannerNextBtn, 'display', 'none');
+  } else {
+    css(swiperBannerNextBtn, 'display', 'flex');
+  }
+
+  if (swiperBannerPrevBtn.classList.contains('swiper-button-disabled')) {
+    css(swiperBannerPrevBtn, 'display', 'none');
+  } else {
+    css(swiperBannerPrevBtn, 'display', 'flex');
+  }
+
+  // const node = e.target;
+  // disableSliderBtn(node);
+}
+
+// function disableSliderBtn(node) {
+//   if (node.classList.contains('swiper-button-disabled')) {
+//     console.log(node + ' : if');
+//     css(node, 'display', 'none');
+//   } else {
+//     console.log(node + ' : else');
+//     css(node, 'display', 'flex');
+//   }
+// }
+
+window.addEventListener('load', handleSliderBtn);
+swiperBannerPrevBtn.addEventListener('click', handleSliderBtn);
+swiperBannerNextBtn.addEventListener('click', handleSliderBtn);
