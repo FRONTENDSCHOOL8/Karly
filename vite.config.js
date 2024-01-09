3fd3846250b439d9179678191e96efa6fb6a2b15
lumpyspaceyz <yzever99@gmail.com>
2024-01-09 14:11:07 +0900

    [modify] 배포용 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,17 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, '/index.html'),
-        cart: resolve(__dirname, '/src/pages/cart/index.html'),
-        login: resolve(__dirname, '/src/pages/login/index.html'),
+        main: resolve(__dirname, 'index.html'),
+        cart: resolve(__dirname, 'src/pages/cart/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
         product_detail: resolve(
           __dirname,
-          '/src/pages/product_detail/index.html'
+          'src/pages/product_detail/index.html'
         ),
-        product_list: resolve(__dirname, '/src/pages/product_list/index.html'),
-        register: resolve(__dirname, '/src/pages/register/index.html'),
-        header: resolve(__dirname, '/src/components/header/index.html'),
-        footer: resolve(__dirname, '/src/components/footer/index.html'),
+        product_list: resolve(__dirname, 'src/pages/product_list/index.html'),
+        register: resolve(__dirname, 'src/pages/register/index.html'),
+        header: resolve(__dirname, 'src/components/header/index.html'),
+        footer: resolve(__dirname, 'src/components/footer/index.html'),
       },
     },
   },
